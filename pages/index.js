806d58d75c1f5771eba7097d8939983950af6948
timetxt <jasonsh@timetxt.com>
2025-01-28 12:19:56 +1100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: '20px',
+      maxWidth: '800px',
+      margin: '0 auto',
+      textAlign: 'center'
+    }}>
+      <h1>Welcome to My Static Site</h1>
+      <p>This is a simple static website built with Next.js</p>
+    </div>
+  )
+}
